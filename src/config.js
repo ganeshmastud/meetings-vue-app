a916b6e43e79714e6c4f5371da0ad27f1c156580
ganeshmastud <mastudganesh6@gmail.com>
2021-10-11 13:37:38 +0530

    refresh page after excusing yourself

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
+import {store} from '@/stores';
 const AppConfig = {
   apiBaseUrl: "https://mymeetingsapp.herokuapp.com/api/",
-  apiToken: this.$store.state.auth.token
+  apiToken: store.state.auth.token
 // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdhbmVzaG1hc3R1ZEBnbWFpbC5jb20iLCJ1c2VySWQiOiI2MTQxNmYzNTFmNjVjNTAwMTViNGM4NjUiLCJpYXQiOjE2MzM4NTkzMjQsImV4cCI6MTYzMzk0NTcyNH0.px7c3ta4Lyp9-rvnp4diOcF1Aht2TQ-U9NJu9XIPJvs"
 };
 
